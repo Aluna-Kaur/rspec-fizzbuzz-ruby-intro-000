a48d1ba70f728b7e2fc186517003f4026536a12b
Aluna Kaur <aluna.singh89@gmail.com>
2021-03-05 03:44:38 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,10 +3,10 @@
 def fizzbuzz(number)
   if number % 3 == 0 && number % 5 == 0
     puts "FizzBuzz"
-  elsif number % 5 == 0
-    puts "Buzz"
   elsif number % 3 == 0
     puts "Fizz"
+  elsif number % 5 == 0
+    puts "Buzz"
   else
     puts nil
   end
